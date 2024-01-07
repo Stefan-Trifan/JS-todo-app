3a2c2b5ecc4eca80a2d782d514bbaaccfb378236
Stefan <trfstephan@gmail.com>
2024-01-07 19:32:45 +0100

    store ejecución

---
+++ src/store/todo.store.js
@@ -0,0 +1,26 @@
+import { Todo } from "../todos/models/todo.model";
+
+const Filters = {
+    All: 'all',
+    Completed: 'Completed',
+    Pending: 'Pending'
+}
+
+const state = {
+    todos: [
+        new Todo('Pieda del alma'),
+        new Todo('Pieda del infinito'),
+        new Todo('Pieda del tiempo'),
+    ],
+    filter: 'Filters.All',
+}
+
+const initStore = () => {
+    console.log(state)
+    console.log('InitStore 🥑')
+}
+
+// Objeto exportado por defecto
+export default {
+    initStore,
+}
